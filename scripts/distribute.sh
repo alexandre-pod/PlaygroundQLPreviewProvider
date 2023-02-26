e821a0fa450fda23f4ef6b38d8ae293b35f25ddf
alexandre-pod <24512899+alexandre-pod@users.noreply.github.com>
2023-02-27 00:03:33 +0100

    Add script to automate distribution

---
+++ scripts/distribute.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+####################################################################################################
+# How to run:
+#
+# cp script/.env.template script/.env
+# # Fill script/.env.template
+# source script/.env
+# ./script/distribute.sh
+####################################################################################################
+
+set -e
+set -o xtrace
+
+cd "$(dirname "$0")"
+cd ..
+
+OUTPUT_DIR="output"
+ARCHIVE_PATH="$OUTPUT_DIR/archive.xcarchive"
+
+xcodebuild archive -scheme PlaygroundQLPreviewProvider \
+	-archivePath "$ARCHIVE_PATH" \
+	-destination "generic/platform=macOS" \
+	-derivedDataPath "tmp_derived_data"
+
+xcodebuild -exportArchive \
+	-archivePath "$ARCHIVE_PATH" \
+	-exportOptionsPlist "scripts/exportOptions.plist" \
+	-allowProvisioningUpdates \
+	-authenticationKeyIssuerID "$API_KEY_ISSUER" \
+	-authenticationKeyID "$API_KEY_ID" \
+	-authenticationKeyPath "$API_KEY_PATH" \
+	-exportPath "$OUTPUT_DIR"
+
+GENERATED_APP="$OUTPUT_DIR/PlaygroundQLPreviewProvider.app"
+
+cp -r "$GENERATED_APP" "$GENERATED_APP.backup"
+
+pushd "$OUTPUT_DIR"
+zip -r "PlaygroundQLPreviewProvider-notarize.zip" PlaygroundQLPreviewProvider.app
+popd
+
+xcrun notarytool submit "$OUTPUT_DIR/PlaygroundQLPreviewProvider-notarize.zip" \
+	--key $API_KEY_PATH \
+	--key-id $API_KEY_ID \
+	--issuer $API_KEY_ISSUER \
+	--wait
+xcrun stapler staple "$GENERATED_APP"
+
+pushd "$OUTPUT_DIR"
+zip -r "../release.zip" PlaygroundQLPreviewProvider.app
+popd
+
+echo 'SUCCESSFUL Release !'
+echo 'The app is present in "release.zip"'
